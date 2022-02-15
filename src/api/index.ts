1300f307d7ec742cca33023a5789f0f8127a518d
Logan Brock <logan.g.brock@gmail.com>
2022-02-14 19:00:35 -0500

    first hour - start

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,27 +2,26 @@
  * Graph definitions and corresponding resolvers
  */
 
-import { gql } from "apollo-server";
+import { gql } from 'apollo-server';
 
 export const typeDefs = gql`
-  type Author {
-    id: ID!
-    givenName: String!
-    familyName: String!
-  }
+	type Author {
+		id: ID!
+		givenName: String!
+		familyName: String!
+	}
 
-  type Query {
-    authors: [Author!]!
-  }
+	type Query {
+		authors: [Author!]!
+	}
 `;
 
 export const resolvers = {
-  Query: {
-    authors: () => {
-      
-      // 🐞 Bug fix needed!
-      // We're not returning what's in DB 😱
-      return [];
-    },
-  },
+	Query: {
+		authors: () => {
+			// 🐞 Bug fix needed!
+			// We're not returning what's in DB 😱
+			return [];
+		},
+	},
 };
